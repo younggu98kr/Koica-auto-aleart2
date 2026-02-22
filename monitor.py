2dd665b183f5aeb338618b31c9064b78f46ec535
Youngju <younggu98kr@hanmail.net>
2026-02-22 14:22:54 +0900

    Update URL and keywords in monitor.py

--- monitor.py
+++ monitor.py
@@ -5,10 +5,10 @@ from urllib.parse import urljoin
 
 import requests
 
-LIST_URL = "http://bike.passo.co.kr/bike/index.php?part=cybershop&path=cybershop&mode=home"
+LIST_URL = "https://job.koica.go.kr/application/applicationListPage.do?menuId=MENU0098"
 
 # 찾고 싶은 키워드들 (원하면 더 추가 가능)
-KEYWORDS = ["PCX", "혼다", "가와사키", "골드윙", "할리"]
+KEYWORDS = ["모집", "자동차", "전문가", "스리랑카", "직업훈련원"]
 
 def fetch_html(url: str) -> str:
     # 웹사이트가 “봇이네?” 하고 튕길 때를 줄이려고 사용자 브라우저처럼 보이게 함
